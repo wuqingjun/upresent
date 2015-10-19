b0d477d2b6ae2ac8bffa089ced1d2f62604b69d1
wadewu <wuqingjun@gmail.com>
2015-10-19 12:46:27 -0700

    changed

--- upresent/routes/fastleet.js
+++
@@ -1,9 +0,0 @@
-﻿var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('/public/fastleet/index.html');
-});
-
-module.exports = router;
