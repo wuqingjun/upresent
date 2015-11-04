1c22eb278b6bbee19751309e29f5d19ff19e5fa2
wadewu <wuqingjun@gmail.com>
2015-11-03 22:11:43 -0800

    fixed undefined key pressed

--- upresent.azure/public/javascripts/checkKey.js
+++ upresent.azure/public/javascripts/checkKey.js
@@ -9,34 +9,37 @@ function min(a, b) { return a <= b ? a : b; }
 
 function checkKey(e) {
     e = e || window.event;
-    
+
+    var keyNum = parseInt(e.keyCode);
+    if (keyNum >= 37 && keyNum <= 40) {
     var url = window.location.pathname;   
     var segments = url.split('/');
     var idx = segments.indexOf(fastleet);
     if (idx >= 0) {
-        var prev = "", next = "", up = "", down = "", pagenumber = 0, cmd = 'q';
-        if (idx < segments.length - 1) {
-            pagenumber = segments[idx + 1];
-        }
-        if (idx < segments.length - 2) {
-            cmd = segments[idx + 2];
-        }
+            var prev = "", next = "", up = "", down = "", pagenumber = 0, cmd = 'q';
+            if (idx < segments.length - 1) {
+                pagenumber = segments[idx + 1];
+            }
+            if (idx < segments.length - 2) {
+                cmd = segments[idx + 2];
+            }
         
-        if (pagenumber && pagenumber.length > 0) {
-            var temp = (pagenumber - 1 + maxpage) % maxpage;
-            prev = "fastleet/" + temp + "/q";
-            temp = (parseInt(pagenumber) + 1) % maxpage;
-            next = "fastleet/" + temp + '/q';
-            up = "fastleet/" + pagenumber + '/' + commands[max(pageMapping[cmd] - 1, 0)];
-            down = "fastleet/" + pagenumber + '/' + commands[min(pageMapping[cmd] + 1, commands.length - 1)];           
-        } else {
-            prev = fastleet;
-            next = fastleet + "/1";
-            up = fastleet;
-            down = fastleet;
-        }
+            if (pagenumber && pagenumber.length > 0) {
+                var temp = (pagenumber - 1 + maxpage) % maxpage;
+                prev = "fastleet/" + temp + "/q";
+                temp = (parseInt(pagenumber) + 1) % maxpage;
+                next = "fastleet/" + temp + '/q';
+                up = "fastleet/" + pagenumber + '/' + commands[max(pageMapping[cmd] - 1, 0)];
+                down = "fastleet/" + pagenumber + '/' + commands[min(pageMapping[cmd] + 1, commands.length - 1)];           
+            } else {
+                prev = fastleet;
+                next = fastleet + "/1";
+                up = fastleet;
+                down = fastleet;
+            }
         
-        var keyHandler = { '37': prev, '38': up, '39': next, '40': down };
-        window.location.pathname = keyHandler[e.keyCode];
+            var keyHandler = { '37': prev, '38': up, '39': next, '40': down };
+            window.location.pathname = keyHandler[e.keyCode];
+        }
     }
 }
